70c131590ca117650b27c27b2082c0f7643fa926
Manon Le <babyvert13@hotmail.com>
2020-12-24 16:30:52 +0100

    change serverUrl

--- src/tools/globalVariables.js
+++ src/tools/globalVariables.js
@@ -1,2 +1,2 @@
-export const serverUrl = 'http://192.168.1.86:3000'
-// export const serverUrl = 'https://serene-coast-48705.herokuapp.com'
+// export const serverUrl = 'http://192.168.1.86:3000'
+export const serverUrl = 'https://serene-coast-48705.herokuapp.com'
